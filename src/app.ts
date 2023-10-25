92fd7a3c99dac4b6fb87046d717da732d8076693
Chesney Nyffels <chesney@nyffels.be>
2023-10-25 09:42:42 +0200

    Adjusted logic to work async

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const password = args.find((a) => a.includes('--dbpassword='))?.replace('--dbpas
 const database = args.find((a) => a.includes('--dbdatabase='))?.replace('--dbdatabase=', '');
 
 if ((host ?? '').length <= 0 || (user ?? '').length <= 0 || (password ?? '').length <= 0 || (database ?? '').length <= 0) {
-  throw Error('Missing database credentials. Consult documentation for more information.')
+  throw Error('Missing database credentials. Consult documentation for more information.');
 }
 
 const connection = mysql.createConnection({
@@ -22,15 +22,18 @@ const connection = mysql.createConnection({
   password: password,
   database: database,
 });
+Run(connection);
 
-switch (action) {
-  case 'dump':
-    connection.end();
-    throw Error('Not yet implemented');
-  case 'restore':
-    connection.end();
-    throw Error('Not yet implemented');
-  default:
-    connection.end();
-    throw Error('No valid action defined. Please use the --action flag to define a valid action! For more information consult the documentation.');
+async function Run(connection: mysql.Connection) {
+  switch (action) {
+    case 'dump':
+      connection.end();
+      throw Error('Not yet implemented');
+    case 'restore':
+      connection.end();
+      throw Error('Not yet implemented');
+    default:
+      connection.end();
+      throw Error('No valid action defined. Please use the --action flag to define a valid action! For more information consult the documentation.');
+  }
 }
