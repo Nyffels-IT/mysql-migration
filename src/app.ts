915cc815e32e29ad3a76f047d506a67407e7cece
Chesney Nyffels <chesney@nyffels.be>
2023-10-25 09:37:32 +0200

    Added database flags

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,36 @@
 #! /usr/bin/env node
 
+import mysql from 'mysql2';
+import _ from 'lodash';
+
 const args = process.argv.slice(2);
 const action = args.find((a) => a.includes('--action='))?.replace('--action=', '');
-console.log(args);
-console.log(action);
+const host = args.find((a) => a.includes('--dbhost='))?.replace('--dbhost=', '');
+const port = args.find((a) => a.includes('--dbport='))?.replace('--dbport=', '');
+const user = args.find((a) => a.includes('--dbuser='))?.replace('--dbuser=', '');
+const password = args.find((a) => a.includes('--dbpassword='))?.replace('--dbpassword=', '');
+const database = args.find((a) => a.includes('--dbdatabase='))?.replace('--dbdatabase=', '');
+
+if ((host ?? '').length <= 0 || (user ?? '').length <= 0 || (password ?? '').length <= 0 || (database ?? '').length <= 0) {
+  throw Error('Missing database credentials. Consult documentation for more information.')
+}
+
+const connection = mysql.createConnection({
+  host: host,
+  port: _.isNil(port) ? 3306 : +port,
+  user: user,
+  password: password,
+  database: database,
+});
 
 switch (action) {
   case 'dump':
-    throw Error("Not yet implemented");
+    connection.end();
+    throw Error('Not yet implemented');
   case 'restore':
-    throw Error("Not yet implemented");
+    connection.end();
+    throw Error('Not yet implemented');
   default:
-    throw Error('No valid action defined. Please use the --action flag to define a action! For more information consult the documentation.');
+    connection.end();
+    throw Error('No valid action defined. Please use the --action flag to define a valid action! For more information consult the documentation.');
 }
